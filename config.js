a25a48f631bd738cd41c00dd0cf64e0d87f102a8
Danghiendevbot001 <135580941+Trandang-hiendev@users.noreply.github.com>
2024-03-26 17:03:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,21 +1,20 @@
-
 require("dotenv").config();
 
 module.exports = {
   token: process.env.TOKEN || '', // your discord bot token
   prefix: process.env.PREFIX || '.', // bot prefix
-  ownerID: process.env.OWNERID || [''], //your discord id
+  ownerID: process.env.OWNERID || ['1114889680484315206'], //your discord id
  nonPrefixedUsers: [""], // Non prefixed users id.
     
-  SpotifyID: process.env.SPOTIFYID || 'e6f84fbec2b44a77bf35a20c5ffa54b8', // spotify client id
-  SpotifySecret: process.env.SPOTIFYSECRET || '498f461b962443cfaf9539c610e2ea81', // spotify client secret
+  SpotifyID: process.env.SPOTIFYID || 'd4ba8f36fe9c433a8d4daa9468479828', // spotify client id
+  SpotifySecret: process.env.SPOTIFYSECRET || '1b587c9a2ee24e6f91663a4215f2180e', // spotify client secret
   mongourl:
     process.env.MONGO_URI || 'mongodb+srv://pee33:wnn99e@cluster0.fgivnid.mongodb.net/?retryWrites=true&w=majority', // MongoDb URL
   embedColor: process.env.COlOR || 'GREY', // embed colour
-  logs: process.env.LOGS || '1198586029343518820', // Discord channel id 
+  logs: process.env.LOGS || '1221700889115623464', // Discord channel id 
   links: {
-    support: 'https://discord.gg/aMC2e8zgQb',
-    invite: 'https://discord.com/api/oauth2/authorize?client_id=1027828697828433980&permissions=279230934336&scope=bot',
+    support: 'https://discord.gg/',
+    invite: 'https://discord.com/oauth2/authorize?client_id=1221699838186360884&permissions=8&scope=bot',
     vote: 'https://top.gg/bot/794917868906086411/vote',
   },
 
